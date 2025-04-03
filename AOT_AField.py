d380e4d27579339ed79f486231a327ad857257e0
Lucas Duclos <lucas.duclos@universite-paris-saclay.fr>
2025-04-03 21:00:44 +0200

    Update AOT_AField.py

--- AOT_AField.py
+++ AOT_AField.py
@@ -1,79 +1,3 @@
-def load_fieldKWAVE(hdr_path):
-    """
-    Lit un fichier Interfile (.hdr) et son fichier binaire (.img) pour reconstruire un champ acoustique.
-
-    Paramètres :
-    ------------
-    - folderPathBase : dossier de base contenant les fichiers
-    - hdr_path : chemin relatif du fichier .hdr depuis folderPathBase
-
-    Retour :
-    --------
-    - field : tableau NumPy contenant le champ acoustique avec les dimensions réordonnées en (X, Z, time)
-    - header : dictionnaire contenant les métadonnées du fichier .hdr
-    """
-    header = {}
-    # Lecture du fichier .hdr
-    with open(hdr_path, 'r') as f:
-        for line in f:
-            if ':=' in line:
-                key, value = line.split(':=', 1)
-                key = key.strip().lower().replace('!', '')
-                value = value.strip()
-                header[key] = value
-
-
-    # Récupère le nom du fichier .img associé
-    data_file = header.get('name of data file') or header.get('name of date file')
-    if data_file is None:
-        raise ValueError(f"Impossible de trouver le fichier de données associé au fichier header {hdr_path}")
-    img_path = os.path.join(os.path.dirname(hdr_path),os.path.basename(data_file))
-
-    # Détermine la taille du champ à partir des métadonnées
-    shape = [int(header[f'matrix size [{i}]']) for i in range(1, 4) if f'matrix size [{i}]' in header]
-    if not shape:
-        raise ValueError("Impossible de déterminer la forme du champ acoustique à partir des métadonnées.")
-
-    # Type de données
-    data_type = header.get('number format', 'short float').lower()
-    dtype_map = {
-        'short float': np.float32,
-        'float': np.float32,
-        'int16': np.int16,
-        'int32': np.int32,
-        'uint16': np.uint16,
-        'uint8': np.uint8
-    }
-    dtype = dtype_map.get(data_type)
-    if dtype is None:
-        raise ValueError(f"Type de données non pris en charge : {data_type}")
-
-    # Ordre des octets (endianness)
-    byte_order = header.get('imagedata byte order', 'LITTLEENDIAN').lower()
-    endianess = '<' if 'little' in byte_order else '>'
-
-    # Vérifie la taille réelle du fichier .img
-    img_size = os.path.getsize(img_path)
-    expected_size = np.prod(shape) * np.dtype(dtype).itemsize
-    if img_size != expected_size:
-        raise ValueError(f"La taille du fichier img ({img_size} octets) ne correspond pas à la taille attendue ({expected_size} octets).")
-
-    # Lecture des données binaires
-    with open(img_path, 'rb') as f:
-        data = np.fromfile(f, dtype=endianess + np.dtype(dtype).char)
-
-    # Reshape les données en (time, Z, X)
-    field = data.reshape(shape[::-1])  # NumPy interprète dans l'ordre C (inverse de MATLAB)
-
-
-
-    # Applique les facteurs d'échelle si disponibles
-    rescale_slope = float(header.get('data rescale slope', 1))
-    rescale_offset = float(header.get('data rescale offset', 0))
-    field = field * rescale_slope + rescale_offset
-
-    return field
-
 def load_fieldHYDRO_XZ(file_path_h5, param_path_mat):    
 
     # Charger les fichiers .mat
@@ -161,7 +85,7 @@ def load_fieldHYDRO_YZ(file_path_h5, param_path_mat):
     envelope_transposed = np.transpose(envelope, (2, 0, 1))
     return envelope_transposed, y_range, z_range
 
-def load_fieldHYDRO_XYZ(file_path_h5, param_path_mat):
+def load_fieldHydro_XYZ(file_path_h5, param_path_mat):
     # Load parameters from the .mat file
     param = scipy.io.loadmat(param_path_mat)
 
@@ -223,3 +147,247 @@ def load_fieldHYDRO_XYZ(file_path_h5, param_path_mat):
             EnveloppeField[:, y, z, :] = np.abs(hilbert(reorganized_data[:, y, z, :], axis=1))
 
     return EnveloppeField.T, x_range, y_range, z_range
+
+def generate_2Dacoustic_field_KWAVE(folderPathBase,depth_end, angle_deg, active_listString, c0=1540, num_elements = 192, num_cycles = 4, element_width = 0.2/1000, depth_start = 0, f_US = 180e6, f_aq = 10e6, IsSaving=True):
+    active_listbin = ''.join(f"{int(active_listString[i:i+2], 16):08b}" for i in range(0, len(active_listString), 2))
+    active_list = np.array([int(char) for char in active_listbin])
+    print(active_list.shape)
+    # Grille
+    probeWidth = num_elements * element_width
+    Xrange = [-20 / 1000, 20 / 1000]  # Plage en X en mètres
+    Zrange = [depth_start, depth_end ]  # Z range in meters for 289 time samples and 10° max
+
+    t0 = floor(Zrange[0]/f_US)
+    tmax = ceil((depth_end -depth_start + probeWidth*sin(radians(angle_deg)))/(c0*cos(radians(angle_deg)))*f_US)
+
+    Nx = ceil((Xrange[1] - Xrange[0]) / element_width)
+    Nz = ceil((Zrange[1] - Zrange[0]) / element_width)
+    Nt = tmax - t0 + 1
+
+    dx = element_width
+    dz = dx
+
+    # Print the results
+    print("Xrange:", Xrange)
+    print("Zrange:", Zrange)
+    print("Nx:", Nx)
+    print("Nz:", Nz)
+    print("dx:",dx)
+    print("dz:",dz)
+    print("Angles : ",angle_deg)
+    print("Active List : ",active_listString)
+
+    kgrid = kWaveGrid([Nx, Nz], [dx, dz])
+    kgrid.setTime(Nt = Nt, dt = 1/f_US)
+
+    inputFileName = os.path.join(folderPathBase,"/KwaveIN.h5")
+    outputFileName = os.path.join(folderPathBase,"/KwaveOUT.h5")
+
+    # Définir le medium
+    # medium = kWaveMedium(sound_speed=1540, density=1000, alpha_coeff=0.75, alpha_power=1.5, BonA=6)
+    medium = kWaveMedium(sound_speed=c0)
+    
+    acoustic_field = np.zeros((kgrid.Nt, Nz, Nx))
+    
+    # Génération du signal de base
+    signal = tone_burst(1.5 / kgrid.dt, f_US, num_cycles).squeeze() # * 1.5 pour faire comme dans Field2
+
+    # Masque de la sonde : alignée dans le plan XZ
+    source = kSource()
+    source.p_mask = np.zeros((Nx, Nz))  # Crée une grille vide pour le masque de la source
+ 
+    # Placement des transducteurs actifs dans le masque
+    for i in range(num_elements):
+        if active_list[i] == 1:  # Vérifiez si l'élément est actif
+            x_pos = i  # Position des éléments sur l'axe X
+            source.p_mask[x_pos, 0] = 1  # Position dans le plan XZ
+
+    source.p_mask = source.p_mask.astype(int)  # Conversion en entier
+    # print("Number of active elements in p_mask:", np.sum(source.p_mask))
+    is_positive_angle = angle_deg >= 0
+    # Inclinaison de la sonde (en degrés)
+    angle_rad = np.radians(abs(angle_deg))  # Convertir en radians
+
+    delayed_signals = apply_delay(signal, num_elements, element_width, c0, angle_rad, kgrid.dt, is_positive_angle)
+
+    # Filtrer les signaux pour correspondre aux éléments actifs
+    delayed_signals_active = delayed_signals[active_list == 1, :]
+    source.p = delayed_signals_active  # Transposer pour que chaque colonne corresponde à un élément actif
+
+    # === Définir les capteurs pour observer les champs acoustiques ===
+    sensor = kSensor()
+    sensor.mask = np.ones((Nx, Nz))  # Capteur couvrant tout le domaine
+
+    # === Options de simulation ===
+    simulation_options = SimulationOptions(
+        data_cast="single",
+        pml_inside=False,  # Empêche le PML d'être ajouté à l'intérieur de la grille
+        pml_x_size=20,      # Taille de la PML sur l'axe X
+        pml_z_size=20,       # Taille de la PML sur l'axe Z  
+        use_sg=False,           # Pas de Staggered Grid         
+        save_to_disk=True,
+        input_filename=inputFileName,
+        output_filename=outputFileName,
+        data_path=folderPathBase)
+
+    execution_options = SimulationExecutionOptions(is_gpu_simulation=True)  # True si GPU disponible
+
+    # === Lancer la simulation ===
+    print("Lancement de la simulation...")
+    sensor_data = kspaceFirstOrder2D(
+        kgrid=kgrid,
+        medium=medium,
+        source=source,
+        sensor=sensor,
+        simulation_options=simulation_options,
+        execution_options=execution_options,
+    )
+    print("Simulation terminée avec succès.")
+
+    acoustic_field = sensor_data['p'].reshape(kgrid.Nt,Nz, Nx)
+    print("Calcul de l'enveloppe acoustique...")
+    
+    for y in range(acoustic_field.shape[2]):
+        acoustic_field[:, :, y, :]= np.abs(hilbert(acoustic_field[:, :, y, :], axis=0))
+
+    acoustic_envelope_squared = np.sum(acoustic_field, axis=2)**2
+
+    if f_US != f_aq:
+        downsample_factor = int(f_US / f_aq)
+    else:
+        downsample_factor = 1    
+
+    acoustic_field_ToSave = acoustic_envelope_squared[::downsample_factor, :, :]
+    
+    if IsSaving:
+        print("Saving...")
+        save_field(acoustic_field_ToSave, num_elements, active_list, angle_deg, folderPathBase, dx, f_aq,(len(signal)-1)*kgrid.dt)
+    return acoustic_field_ToSave
+    
+
+def generate_3Dacoustic_field_KWAVE(folderPathBase,depth_end, angle_deg, active_listString, c0=1540, num_elements = 192, num_cycles = 4, element_width = 0.2/1000, element_height = 6/1000, depth_start = 0, f_US = 180e6, f_aq = 10e6, IsSaving=True):
+
+    active_listbin = ''.join(f"{int(active_listString[i:i+2], 16):08b}" for i in range(0, len(active_listString), 2))
+    active_list = np.array([int(char) for char in active_listbin])
+    print(active_list.shape)
+    # Grille
+    probeWidth = num_elements * element_width
+    Xrange = [-20 / 1000, 20 / 1000]  # Plage en X en mètres
+    Yrange = [-element_height * 5 / 2, element_height * 5 / 2]  # Plage en Y en mètres
+    Zrange = [depth_start, depth_end]  # Plage en Z en mètres
+
+    t0 = floor(Zrange[0]/f_US)
+    tmax = ceil((depth_end -depth_start + probeWidth*sin(radians(angle_deg)))/(c0*cos(radians(angle_deg)))*f_US)
+
+    dx = element_width
+    dz = dx
+    dy = dx
+
+    Nx = ceil((Xrange[1] - Xrange[0]) / dx)
+    Ny = 4 * ceil((Yrange[1] - Yrange[0]) / element_height)
+    Nz = ceil((Zrange[1] - Zrange[0]) / dz)
+    Nt = tmax - t0 + 1
+
+    # Print the results
+    print("Xrange:", Xrange)
+    print("Yrange:", Yrange)
+    print("Zrange:", Zrange)
+    print("Nx:", Nx)
+    print("Ny:", Ny)
+    print("Nz:", Nz)
+    print("dx:",dx)
+    print("dy:",dy)
+    print("dz:",dz)
+    print("Angles : ",angle_deg)
+    print("Active List : ",active_listString)
+
+    kgrid = kWaveGrid([Nx, Ny, Nz], [element_width, element_height, dx])
+    kgrid.setTime(Nt = Nt, dt = 1/f_US)
+
+    inputFileName = os.path.join(folderPathBase,"/KwaveIN.h5")
+    outputFileName = os.path.join(folderPathBase,"/KwaveOUT.h5")
+
+    # Définir le medium
+    # medium = kWaveMedium(sound_speed=1540, density=1000, alpha_coeff=0.75, alpha_power=1.5, BonA=6)
+    medium = kWaveMedium(sound_speed=c0)
+    
+    acoustic_field = np.zeros((kgrid.Nt, Nz, Ny, Nx))
+    acoustic_envelope_squared = np.zeros((kgrid.Nt, Nz, Nx))
+    
+    # Génération du signal de base
+    signal = tone_burst(1.5 / kgrid.dt, f_US, num_cycles).squeeze() # * 1.5 pour faire comme dans Field2
+
+    # Masque de la sonde : alignée dans le plan XZ
+    source = kSource()
+    source.p_mask = np.zeros((Nx, Ny, Nz))  # Crée une grille vide pour le masque de la source
+
+    stringList = ''.join(map(str, active_list))
+    print(stringList)
+ 
+    # Placement des transducteurs actifs dans le masque
+    for i in range(num_elements):
+        if active_list[i] == 1:  # Vérifiez si l'élément est actif
+            x_pos = i  # Position des éléments sur l'axe X
+            source.p_mask[x_pos, Ny // 2, 0] = 1  # Position dans le plan XZ
+
+    source.p_mask = source.p_mask.astype(int)  # Conversion en entier
+    # print("Number of active elements in p_mask:", np.sum(source.p_mask))
+    is_positive_angle = angle_deg >= 0
+    # Inclinaison de la sonde (en degrés)
+    angle_rad = np.radians(abs(angle_deg))  # Convertir en radians
+
+    delayed_signals = apply_delay(signal, num_elements, element_width, c0, angle_rad, kgrid.dt, is_positive_angle)
+
+    # Filtrer les signaux pour correspondre aux éléments actifs
+    delayed_signals_active = delayed_signals[active_list == 1, :]
+    source.p = delayed_signals_active  # Transposer pour que chaque colonne corresponde à un élément actif
+
+    # === Définir les capteurs pour observer les champs acoustiques ===
+    sensor = kSensor()
+    sensor.mask = np.ones((Nx, Ny, Nz))  # Capteur couvrant tout le domaine
+
+    # === Options de simulation ===
+    simulation_options = SimulationOptions(
+        data_cast="single",
+        pml_inside=False,  # Empêche le PML d'être ajouté à l'intérieur de la grille
+        pml_x_size=20,      # Taille de la PML sur l'axe X
+        pml_y_size=2,      # Taille de la PML sur l'axe Y
+        pml_z_size=20,       # Taille de la PML sur l'axe Z  
+        use_sg=False,           # Pas de Staggered Grid         
+        save_to_disk=True,
+        input_filename=inputFileName,
+        output_filename=outputFileName,
+        data_path=folderPathBase)
+
+    execution_options = SimulationExecutionOptions(is_gpu_simulation=True)  # True si GPU disponible
+
+    # === Lancer la simulation ===
+    print("Lancement de la simulation...")
+    sensor_data = kspaceFirstOrder3D(
+        kgrid=kgrid,
+        medium=medium,
+        source=source,
+        sensor=sensor,
+        simulation_options=simulation_options,
+        execution_options=execution_options,
+    )
+    print("Simulation terminée avec succès.")
+    
+    acoustic_field = sensor_data['p'].reshape(kgrid.Nt,Nz, Ny, Nx)
+
+    print("Calcul de l'enveloppe acoustique...")
+    for y in range(acoustic_field.shape[2]):
+        acoustic_field[:, :, y, :]= np.abs(hilbert(acoustic_field[:, :, y, :], axis=0))
+    acoustic_envelope_squared = np.sum(acoustic_field, axis=2)**2
+    if f_US != f_aq:
+        downsample_factor = int(f_US / f_aq)
+    else:
+        downsample_factor = 1    
+
+    acoustic_field_ToSave = acoustic_envelope_squared[::downsample_factor, :, :]
+    
+    if IsSaving:
+        print("Saving...")
+        save_field(acoustic_field_ToSave, num_elements, active_list, angle_deg, folderPathBase, dx, f_aq,(len(signal)-1)*kgrid.dt)
+
+    return acoustic_field_ToSave
