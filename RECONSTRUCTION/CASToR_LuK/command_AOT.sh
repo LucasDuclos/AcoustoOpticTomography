e6ae4e8293ef77a6ad9a049ca2a7fb5766ad767a
Lucas Duclos <lucas.duclos@universite-paris-saclay.fr>
2026-01-20 12:12:32 +0100

    Delete RECONSTRUCTION directory

--- RECONSTRUCTION/CASToR_LuK/command_AOT.sh
+++
@@ -1,1 +0,0 @@
-castor-recon -df AOSignals.cdh -opti LDWB -it 100:1 -proj matrix -dout results -th 1 -vb 5 -proj-comp 1 -ignore-scanner -data-type AOT -ignore-corr cali,fdur -system-matrix ../../system_matrix/
